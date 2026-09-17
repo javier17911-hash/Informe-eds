a7025fc820f8c1afc45ba928943bfc303b2cfaf0
javier17911-hash <javier.17911@gmail.com>
2026-09-17 10:21:39 -0500

    Actualizar PWA y cargar función de eliminar documentos

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'informe-eds-v1';
-const APP_SHELL = ['./', './index.html', './manifest.json', './logo.svg'];
+const CACHE_NAME = 'informe-eds-v2';
+const APP_SHELL = ['./', './index.html', './manifest.json', './logo.svg', './delete.js'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
