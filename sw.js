d8a81ad5604e9204b90ba9813194bbc43ce567a0
javier17911-hash <javier.17911@gmail.com>
2026-09-16 23:12:53 -0500

    Actualizar caché de la aplicación

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="informe-eds-v3";
+const CACHE="informe-eds-v4";
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"])).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).then(r=>{if(e.request.method==="GET"){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});}return r}).catch(()=>caches.match(e.request))));
